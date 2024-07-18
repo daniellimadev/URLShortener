48cd268aaccea6224d3ef954b114fde5b96ce1d2
daniellimadev <pdaniel643@gmail.com>
2024-07-18 17:13:14 -0300

    Structuring the project and database

---
+++ src/test/java/com/github/daniellimadev/URLShortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.daniellimadev.URLShortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
